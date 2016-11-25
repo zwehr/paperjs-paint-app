4d0ff90142b54ed14aa8479196ab8f2ddad45078
zwehr <zwehrmeister@gmail.com>
2016-11-24 19:43:56 -0500

    create color choices

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-// Create a Paper.js Path to draw a line into it:
-var path = new Path();
-// Give the stroke a color
-path.strokeColor = 'black';
-var start = new Point(100, 100);
-// Move to start and draw a line from there
-path.moveTo(start);
-// Note the plus operator on Point objects.
-// PaperScript does that for us, and much more!
-path.lineTo(start + [ 100, -50 ]);
+var currentColor = 'black';
+
+function onMouseDown(event) {
+    var circle = Shape.Circle(event.point, 30);
+        circle.fillColor = currentColor;
+}
+
+$(".color-choice").click(function() {
+    currentColor = $(this).css("background-color");
+});
