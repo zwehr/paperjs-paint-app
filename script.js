f7b7cab8a82c30f4210d5d7cb147b6e0ebe0dc24
zwehr <zwehrmeister@gmail.com>
2016-11-25 20:16:18 -0500

    add more colors and display current color on click

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-var currentColor = 'black';
+var currentColor = "black";
+var currentTool = "circle";
+var width = 30;
 
 function onMouseDown(event) {
-    var circle = Shape.Circle(event.point, 30);
+    var circle = Shape.Circle(event.point, width);
         circle.fillColor = currentColor;
 }
 
-$(".color-choice").click(function() {
+$(".color-button").click(function() {
+    //update currentColor variable from clicked background-color
     currentColor = $(this).css("background-color");
+    
+    //display current color above color options
+    $("#current-color").css({backgroundColor: currentColor});
 });
