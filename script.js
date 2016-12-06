423dab17c2cf6e5bed76572844103248d62cd475
zwehr <zwehrmeister@gmail.com>
2016-12-05 23:51:52 -0500

    make hex field/submit functional with jQuery

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var width = 30;
 
 function onMouseDown(event) {
     var circle = Shape.Circle(event.point, width);
-        circle.fillColor = currentColor;
+    circle.fillColor = currentColor;
 }
 
 $(".color-button").click(function() {
@@ -12,6 +12,14 @@ $(".color-button").click(function() {
     currentColor = $(this).css("background-color");
     
     //display current color above color options
+    $("#current-color").css({backgroundColor: currentColor});
+});
+
+$("#apply-hex").click(function() {
+    //add hash to user's hex and set current color
+    currentColor = "#" + $("#hex-value").val();
+    
+    //set #current-color box to user's hex
     $("#current-color").css({backgroundColor: currentColor});
 });
 
@@ -24,7 +32,7 @@ $("#increase-width").click(function() {
         width+= 5;
         $("#current-width").html(width);
     }
-})
+});
 
 $("#decrease-width").click(function() {
     if (width == 0) {
@@ -33,4 +41,4 @@ $("#decrease-width").click(function() {
         width-= 5;
         $("#current-width").html(width);
     }
-})
+});
